60433b19b04d900b4683a8c65392421dc472f46f
Avinash Prajapati <avinashprajapati1611r@gmail.com>
2023-07-31 21:44:40 +0530

    changes

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -6,9 +6,16 @@ public class Pizza {
     private Boolean isVeg;
     private String bill;
 
+    protected boolean hasExtraCheese;
+    protected boolean hasExtraToppings;
+    private boolean hasPaperBag;
+
     public Pizza(Boolean isVeg){
-        this.isVeg = isVeg;
-        // your code goes here
+            this.price = isVeg ? 300 : 400;
+            this.isVeg = isVeg;
+            this.hasExtraCheese = false;
+            this.hasExtraToppings = false;
+            this.hasPaperBag = false;
     }
 
     public int getPrice(){
@@ -17,18 +24,40 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
+        if (!hasExtraCheese) {
+            price += 80;
+            hasExtraCheese = true;
+        }
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if (!hasExtraToppings) {
+            int toppingsPrice = isVeg ? 70 : 120;
+            price+= toppingsPrice;
+            hasExtraToppings = true;
+        }
     }
 
     public void addTakeaway(){
         // your code goes here
+        if (!hasPaperBag) {
+            price += 20;
+            hasPaperBag = true;
+        }
     }
 
     public String getBill(){
         // your code goes here
-        return this.bill;
+        StringBuilder sb=new StringBuilder();
+        sb.append("Base Price Of The Pizza: ").append(isVeg ?"300":"400").append("\n");
+        if(hasExtraCheese)
+        sb.append("Extra Cheese Added: 80"+"\n");
+        if(hasExtraToppings)
+            sb.append("Extra Toppings Added: ").append(isVeg ?"70":"120").append("\n");
+        if(hasPaperBag)
+            sb.append("Paperbag Added: 20"+"\n");
+        sb.append("Total Price: "+price);
+        return sb.toString();
     }
 }
